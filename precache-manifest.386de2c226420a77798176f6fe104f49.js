07d90b660d7ec020678c6cf1eb1cf901bfa64855
201502038 <bnm961126@gmail.com>
2019-08-08 20:13:47 +0900

    Updates

---
+++ precache-manifest.386de2c226420a77798176f6fe104f49.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208cfecbd7a50a704f5fc398d9d3abff",
+    "url": "/Hosting/index.html"
+  },
+  {
+    "revision": "0d7aeb2bce5268e2f238",
+    "url": "/Hosting/static/css/main.86627fcb.chunk.css"
+  },
+  {
+    "revision": "4737ce8c0e0e7b9685c2",
+    "url": "/Hosting/static/js/2.47d0bd72.chunk.js"
+  },
+  {
+    "revision": "0d7aeb2bce5268e2f238",
+    "url": "/Hosting/static/js/main.bb6e9ed1.chunk.js"
+  },
+  {
+    "revision": "ae0324bdaf3551ce2423",
+    "url": "/Hosting/static/js/runtime~main.841a3670.js"
+  },
+  {
+    "revision": "aec347e36de00a06a15e9c824bc3eec9",
+    "url": "/Hosting/static/media/boon.aec347e3.jpg"
+  },
+  {
+    "revision": "e42cb2511479d66c37c0e4335f58d423",
+    "url": "/Hosting/static/media/chicken.e42cb251.png"
+  },
+  {
+    "revision": "18c9b7fe9b0536bfad3e923fee6d647a",
+    "url": "/Hosting/static/media/chinafood.18c9b7fe.png"
+  },
+  {
+    "revision": "c66556a14f6b7dbcd7b7ec4edeae6f37",
+    "url": "/Hosting/static/media/japanfood.c66556a1.jpg"
+  },
+  {
+    "revision": "2be782a5f39df7aec387cf6d70bed5c0",
+    "url": "/Hosting/static/media/main.2be782a5.png"
+  },
+  {
+    "revision": "3a059961dc4fd88eafc8b70a84157c1e",
+    "url": "/Hosting/static/media/main.3a059961.png"
+  },
+  {
+    "revision": "e42cb2511479d66c37c0e4335f58d423",
+    "url": "/Hosting/static/media/main.e42cb251.png"
+  },
+  {
+    "revision": "36b3f2983155286646b8449354e3e3c2",
+    "url": "/Hosting/static/media/pigfoot.36b3f298.jpg"
+  },
+  {
+    "revision": "ad0db1bb4863b071d3135a3b09d9bcfb",
+    "url": "/Hosting/static/media/pin.ad0db1bb.png"
+  },
+  {
+    "revision": "8104832311474c1f7dafbe36ba1021d9",
+    "url": "/Hosting/static/media/pizza.81048323.jpg"
+  },
+  {
+    "revision": "6c3274fc94fc8adf0d386c78ee4a4a93",
+    "url": "/Hosting/static/media/search.6c3274fc.png"
+  },
+  {
+    "revision": "7bec63f1d413b56b71ae05b20340f74d",
+    "url": "/Hosting/static/media/westfood.7bec63f1.jpg"
+  }
+]);
